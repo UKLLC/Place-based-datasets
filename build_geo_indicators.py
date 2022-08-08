0972cd70958b274b0e6820a795d6e860c42a428c
richtea87 <rgt737@gmail.com>
2022-08-08 15:42:23 +0100

    tidying up and generating metadata

--- build_geo_indicators.py
+++ build_geo_indicators.py
@@ -52,9 +52,9 @@ def incremental_range(start, stop, step):
 # data file locations
 dataloc = "L:/Data/geo_data"
 
-# 1a) IMD -  import IMD with subdomains
+# 1a) IMD - data
+# import IMD with subdomains
 imd19sub = pd.read_excel(dataloc+"/IMD/File_2_-_IoD2019_Domains_of_Deprivation.xlsx", sheet_name="IoD2019 Domains")
-
 # CREATE QUANTILES
 # define cuts (quantiles)
 cuts = [3,4,5]
@@ -86,7 +86,6 @@ decs_d = dict(zip(decs, decs_new))
 # rename
 for k, v in decs_d.items():
     imd19sub.rename({k:v}, axis=1, inplace = True)
-    
 # CLEARUP AND STRIP DOWN DF
 # create list of rank vars to drop
 rank = [x for x in imd19sub_cols if "Rank" in x]
@@ -94,9 +93,54 @@ rank = [x for x in imd19sub_cols if "Rank" in x]
 to_drop = ['Local Authority District code (2019)', 'Local Authority District name (2019)','LSOA name (2011)'] + rank
 # drop unrequired cols
 imd19sub = imd19sub.drop(columns=to_drop)
+# rename LSAO 2011 col
+imd19sub.rename({'LSOA code (2011)' : 'LSOA11CD'}, axis=1, inplace=True)
+
+# 1b) IMD metadata
+# only need variable lables (not values for this one)
+# isolate column names
+imd_cols = list(imd19sub.columns)
+imd_cols.remove("LSOA11CD")
+# convert to df
+t10 = pd.DataFrame.from_dict(decs_d, orient='index').reset_index()
+t10.columns = ['variable_label', 'variable_name']
+# create quintile version
+#t5 = t10
+#t5.columns = ['variable_label', 'variable_name']
+# replace in label column
+#t5['variable_label'] = t5['variable_label'].str.replace('10%', '20%')
+#t5['variable_name'] = t5['variable_name'].str.replace('q10', 'q5')
+
+def label_replace(quantile):
+    # calc % element of label
+    perc = int(100/quantile)
+    # take copy of decile dataframe (used as base)
+    df = t10.copy()
+    # renaming and replacing
+    df.columns = ['variable_label', 'variable_name']
+    df['variable_label'] = df['variable_label'].str.replace('10%', str(int(perc))+'%')
+    df['variable_name'] = df['variable_name'].str.replace('q10', 'q'+str(quantile))
+    # return new dataframe
+    return df
+
+# create second list with dataframe names
+cuts2 = ['t'+str(i) for i in cuts]
+# convert to dict
+cuts_d = dict(zip(cuts, cuts2))
+# create empty dict for dataframes
+cut_dfs = {}
+# create empty dataframes to append all together
+cut_all_df = pd.DataFrame()
+# create dict of dataframes
+for k, v in cuts_d.items():
+    cut_dfs[v] = label_replace(k)
+# append together dataframes
+for k, v in cut_dfs.items():
+    cut_all_df = cut_all_df.append(v)
+
 
 ###################
-# 2) pop density
+# 2a) pop density data
 ###################
 pop_den = pd.read_excel(dataloc+"/pop_den/sape23dt11mid2020lsoapopulationdensity.xlsx", sheet_name="Mid-2020 Population Density", skiprows = 4)
 # hist to assess distributio
@@ -105,20 +149,39 @@ pop_den.hist(column='People per Sq Km')
 # first create incremental list
 t1 = list(incremental_range(0, 10100, 100))
 # then create catagorical variable as string
-pop_den['people_km2_mid2020'] = pd.cut(pop_den['People per Sq Km'], bins=t1).astype(str)
+pop_den['people_km2_2020'] = pd.cut(pop_den['People per Sq Km'], bins=t1).astype(str)
 # add topcut
-pop_den['people_km2_mid2020'] = np.where(pop_den['People per Sq Km']>=10000,">=10000",pop_den['people_km2_mid2020'])
+pop_den['people_km2_2020'] = np.where(pop_den['People per Sq Km']>=10000,">=10000",pop_den['people_km2_2020'])
+# cleanup 
+pop_den['people_km2_2020'] = pop_den['people_km2_2020'].str.replace("(","").str.replace("]","").str.replace(","," -")
+
 # value counts
-t2 = pop_den['people_km2_mid2020'].value_counts()
+t2 = pop_den['people_km2_2020'].value_counts()
 # strip down vars
-pop_den = pop_den[['LSOA Code','people_km2_mid2020']]
+pop_den = pop_den[['LSOA Code','people_km2_2020']]
+# rename LSAO 2011 col
+pop_den.rename({'LSOA Code' : 'LSOA11CD'}, axis=1, inplace=True)
 # merge with IMD file
-all_geo = imd19sub.merge(pop_den, how='right', left_on='LSOA code (2011)', right_on='LSOA Code')
+all_geo = imd19sub.merge(pop_den, how='right', on='LSOA11CD')
+
+
+###################
+# 2b) pop density data meta data
+###################
+# variable labels define
+popd_var_labs = {
+    'LSOA11CD' : 'Lower Super Output Areas from 2011 Census',
+    'people_km2_2020' : 'People per Sq Km - mid 2020'}
+# convert to df
+popd_var_labs_df = pd.DataFrame(popd_var_labs.items(), columns=['variable_name', 'variable_label'])
+# value labels - using string rather than value lables here
+
 
 #####################
 # 3) urban rural
 #####################
 # 3a) data
+# import UR 2011 master file
 urb_rur = pd.read_csv(dataloc+"/urb_rur/Rural_Urban_Classification_(2011)_of_Lower_Layer_Super_Output_Areas_in_England_and_Wales.csv")
 # recode - keep letter component only - this is to minimise disc risk where the sparse settting component is included
 urb_rur['RUC11CD_V2'] = urb_rur['RUC11CD'].str[0]
@@ -126,7 +189,7 @@ urb_rur['RUC11CD_V2'].value_counts()
 # strip down actual data dropping unrequireed vars
 urb_rur = urb_rur[['LSOA11CD', 'RUC11CD_V2']]
 # merge with other geo data
-all_geo = all_geo.merge(urb_rur, how='inner', left_on='LSOA code (2011)', right_on='LSOA11CD')
+all_geo = all_geo.merge(urb_rur, how='inner', on='LSOA11CD')
 
 
 # 3b) metadata
